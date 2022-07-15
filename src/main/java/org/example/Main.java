a05bee398fd77cb16fce74e6e9c32126ff990967
lilitoones <liliarevalovargas@gmail.com>
2022-07-14 20:47:55 -0500

    Ejercicio 4 introducción a la programación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,6 +16,7 @@ public class Main {
         }
 
 
+
         //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3
         //el bloque de código que tendrá el bucle deberá:
         //Incrementar el valor de la variable en uno cada vez que se ejecute
