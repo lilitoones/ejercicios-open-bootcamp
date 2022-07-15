3c25cfa9101a0108a166a8a80d1a426a715c3bc0
lilitoones <liliarevalovargas@gmail.com>
2022-07-14 20:50:01 -0500

    Ejercicio 4 introducción a la programación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,8 +15,6 @@ public class Main {
             System.out.println("El numero es positivo");
         }
 
-
-
         //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3
         //el bloque de código que tendrá el bucle deberá:
         //Incrementar el valor de la variable en uno cada vez que se ejecute
